89b1e6d0cd3632093dea9a92ddac31a1f412de90
ថಘઅഞפּ ṛཥચ <42241901+arsium@users.noreply.github.com>
2021-12-02 22:58:47 +0100

    #Issue 1
    Refer to : https://github.com/arsium/HookLib/issues/1

--- HookLib.cs
+++ HookLib.cs
@@ -12,14 +12,14 @@ namespace HookLib
         public byte[] OldBytes { get; set; }
         private IntPtr ProcessToHook { get; set; }
 
-        public HookLib(IntPtr ProcessToPatch, string LibName, string FunctionName, byte[] BytesToHook, uint SizeOfBytesToHook)
+        public HookLib(IntPtr ProcessToPatch, string LibName, string FunctionName, byte[] BytesToHook)
         {
-            OldBytes = new byte[SizeOfBytesToHook]; //first we need a buffer to restore old function bytes to unhook it
+            OldBytes = new byte[BytesToHook.Length]; //first we need a buffer to restore old function bytes to unhook it
             ProcessToHook = ProcessToPatch;
             LibToHook = LibName;//the lib ex kernel32 or ntdll
             FunctionToHook = FunctionName;//name of the function you want to hook 
             NewBytes = BytesToHook;//bytes you want to use as replacement of our function address
-            SizeOfNewBytes = SizeOfBytesToHook;//the size of hooked bytes
+            SizeOfNewBytes = (uint)BytesToHook.Length;//the size of hooked bytes
         }
 
         public bool HookedFunction() 
@@ -27,7 +27,7 @@ namespace HookLib
             IntPtr AddressOfLib = NativeAPI.GetModuleHandle(LibToHook);//getting lib address in our program
             IntPtr FunctionAddress = NativeAPI.GetProcAddress(AddressOfLib, FunctionToHook);//getting function address in our program
             NativeAPI.ReadProcessMemory(ProcessToHook, FunctionAddress, OldBytes, SizeOfNewBytes, 0);//read the original bytes from our function address and store them if you want to restore
-            return IsHooked = NativeAPI.WriteProcessMemory(ProcessToHook, FunctionAddress, NewBytes, SizeOfNewBytes, 0);// here we hooked the function : the bytes at function address are replaced by our code (asm or opcode !)
+            return IsHooked = NativeAPI.WriteProcessMemory(ProcessToHook, FunctionAddress, NewBytes, SizeOfNewBytes, 0);// here we hooked the function : the address of our function is replace by our code (asm or opcode !)
         }
      
         public bool UnHookedFunction() 
